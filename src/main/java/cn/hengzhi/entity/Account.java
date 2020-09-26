7098c37769e9e932d03453af2f5afcb5406f309c
aiwoqe <1936563461@qq.com>
2020-09-26 16:29:54 +0800

    本地提交

---
+++ src/main/java/cn/hengzhi/entity/Account.java
@@ -0,0 +1,42 @@
+package cn.hengzhi.entity;
+
+/**
+ * @ Author     ：MI
+ * @ Date       ：Created in 23:51 2019/9/26
+ * @ Description：实体类
+ */
+public class Account {
+    private Integer id;
+    private String name;
+    private double money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public Account(Integer id, String name, double money) {
+        this.id = id;
+        this.name = name;
+        this.money = money;
+    }
+}
